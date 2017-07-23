13db64ff11088494399fbbe7a6c64ffaba2f13b2
Em Klick <eklick@chicagobooth.edu>
2017-07-23 21:55:46 +0000

    4

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,11 +13,11 @@ class MoviesController < ApplicationController
 
   def new_form
     
-    
     render("movies/new_form.html.erb")
   end
 
   def create_row
+    @movie = Movie.new
     @movie.title = params[:the_title]
     @movie.year = params[:the_year]
     @movie.duration = params[:the_duration]
@@ -37,12 +37,15 @@ class MoviesController < ApplicationController
   end
 
   def update_row
-    @movie.title = params[:title]
-    @movie.year = params[:year]
-    @movie.duration = params[:duration]
-    @movie.description = params[:description]
-    @movie.image_url = params[:image_url]
-    @movie.director_id = params[:director_id]
+    
+    @movie = Movie.find(params[:id])    
+    
+    @movie.title = params[:the_title]
+    @movie.year = params[:the_year]
+    @movie.duration = params[:the_duration]
+    @movie.description = params[:the_description]
+    @movie.image_url = params[:the_image_url]
+    @movie.director_id = params[:the_director_id]
 
     render("movies/edit_form.html.erb")
   end
