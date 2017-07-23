5fd95fdf8065f1c4f4f776dffe74dafd46dd4609
Em Klick <eklick@chicagobooth.edu>
2017-07-23 22:13:54 +0000

    6

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,18 @@ Rails.application.routes.draw do
   # Routes for the Director resource:
   # CREATE
   get("/directors/new_form", { :controller => "directors", :action => "new_form" })
-  get("/create_director", { :controller => "director", :action => "create_row" })
+  get("/create_director", { :controller => "directors", :action => "create_row" })
 
   # READ
-  get("/directors", { :controller => "director", :action => "index" })
-  get("/directors/id", { :controller => "director", :action => "show" })
+  get("/directors", { :controller => "directors", :action => "index" })
+  get("/directors/:id", { :controller => "directors", :action => "show" })
 
   # UPDATE
-  get("/directors/:id/edit_form", { :controller => "director", :action => "edit_form" })
-  get("/update_director/:id", { :controller => "director", :action => "update_row" })
+  get("/directors/:id/edit_form", { :controller => "directors", :action => "edit_form" })
+  get("/update_director/:id", { :controller => "directors", :action => "update_row" })
 
   # DELETE
-  get("/delete_director/:id", { :controller => "director", :action => "destroy" })
+  get("/delete_director/:id", { :controller => "directors", :action => "destroy" })
   #------------------------------
 
   # Routes for the Movie resource:
